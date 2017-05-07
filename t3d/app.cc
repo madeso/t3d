b94c8a731bd4443b2d4fe9cce6dfa27a25ad0f1f
Gustav <sir.gustav.the.coder@gmail.com>
2017-05-07 22:44:06 +0200

    added meny

--- t3d/app.cc
+++ t3d/app.cc
@@ -7,23 +7,50 @@ class App: public wxApp
 {
   virtual bool OnInit();
 
-  wxFrame* frame;
-  Pane* pane;
 };
 
 IMPLEMENT_APP(App)
 
+class MainFrame : wxFrame {
+public:
+  MainFrame();
+
+private:
+  Pane* pane;
+};
+
 bool App::OnInit()
 {
-  wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-  frame = new wxFrame((wxFrame *)NULL, -1,  wxT("T3D - Tiled 3d Editor"), wxPoint(50,50), wxSize(400,200));
+  new MainFrame();
+  return true;
+}
 
-  pane = new Pane( (wxFrame*) frame);
+MainFrame::MainFrame()
+    : wxFrame((wxFrame *)NULL, -1,  wxT("T3D - Tiled 3d Editor"), wxPoint(50,50), wxSize(400,200))
+{
+  wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+  pane = new Pane(this);
   sizer->Add(pane, 1, wxEXPAND);
 
-  frame->SetSizer(sizer);
-  frame->SetAutoLayout(true);
+  wxMenuBar* m_pMenuBar = new wxMenuBar();
 
-  frame->Show();
-  return true;
+  // File Menu
+  wxMenu* m_pFileMenu = new wxMenu();
+  m_pFileMenu->Append(wxID_OPEN, _T("&Open"));
+  m_pFileMenu->Append(wxID_SAVE, _T("&Save"));
+  m_pFileMenu->AppendSeparator();
+  m_pFileMenu->Append(wxID_EXIT, _T("&Quit"));
+  m_pMenuBar->Append(m_pFileMenu, _T("&File"));
+
+  // About menu
+  wxMenu* m_pHelpMenu = new wxMenu();
+  m_pHelpMenu->Append(wxID_ABOUT, _T("&About"));
+  m_pMenuBar->Append(m_pHelpMenu, _T("&Help"));
+
+  SetMenuBar(m_pMenuBar);
+
+  SetSizer(sizer);
+  SetAutoLayout(true);
+
+  Show();
 }
